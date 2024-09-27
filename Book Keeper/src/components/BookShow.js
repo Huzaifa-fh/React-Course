1e001bc80cbaaea9f9ce9a3d38fb1ce6a239005a
huzaifa-fh <huzaifabinfaheem1@gmail.com>
2024-09-27 19:03:22 +0500

    Complete Book Keeper

--- Book Keeper/src/components/BookShow.js
+++ Book Keeper/src/components/BookShow.js
@@ -1,6 +1,40 @@
-function BookShow() {
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
+function BookShow({ book, onDelete, onEdit }) {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleDeleteClick = () => {
+        onDelete(book.id);
+    };
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    };
+
+    const handleSubmit = (id, newTitle) => {
+        onEdit(id, newTitle);
+        setShowEdit(!showEdit);
+    };
+
+    let content = <h3>{book.title}</h3>;
+    if (showEdit) {
+        content = <BookEdit book={book} onSubmit={handleSubmit} />;
+    }
+
     return (
-        <div>BookShow</div>
+        <div className="book-show">
+            <img alt="book" src={`https://picsum.photos/seed/${book.id}/200/300`} />
+            <div>{content}</div>
+            <div className="actions">
+                <button className="edit" onClick={handleEditClick}>
+                    Edit
+                </button>
+                <button className="delete" onClick={handleDeleteClick}>
+                    Delete
+                </button>
+            </div>
+        </div>
     );
 }
 
