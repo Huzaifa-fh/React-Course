07462957bce5a0b2b6080d0e325c26ea0dde06c0
huzaifa-fh <huzaifabinfaheem1@gmail.com>
2024-10-07 15:36:45 +0500

    Dropdown Implemented

--- Components Practice/src/App.js
+++ Components Practice/src/App.js
@@ -1,27 +1,25 @@
-import Accordian from './components/Accordion';
+import { useState } from 'react';
+import Dropdown from './components/Dropdown';
 
 function App() {
-    const items = [
-        {
-            id: "dsfdfsda",
-            label: 'Can I use React on a project?',
-            content: 'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.You can use React on any project you want. You can use React on any project you want.'
-        },
-        {
-            id: "43twegeff",
-            label: 'Can I use Javascript on a project?',
-            content: 'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.You can use React on any project you want. You can use React on any project you want.'
-        },
-        {
-            id: "3264yrgewf",
-            label: 'Can I use CSS on a project?',
-            content: 'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.You can use React on any project you want. You can use React on any project you want.'
-        },
-    ]
+  const [selection, setSelection] = useState(null);
 
-    return (
-        <Accordian items={items} />
-    );
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
+  ];
+
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
 }
 
 export default App;
